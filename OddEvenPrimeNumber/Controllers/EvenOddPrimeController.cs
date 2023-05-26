b965b841fab09cfcab3d21a93b67e0f7d627f069
alif-dot <alif17648@gmail.com>
2023-05-26 15:25:14 +0600

    Add project files.

---
+++ OddEvenPrimeNumber/Controllers/EvenOddPrimeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EvenOddPrimeNumber.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EvenOddPrimeController : ControllerBase
+    {
+        [HttpPost]
+        [Route("api/EvenOdd")]
+        public string EvenOdd([FromBody] int n)
+        {
+            if (n % 2 == 0) return "Even";
+            return "Odd";
+        }
+
+        [HttpPost]
+        [Route("api/Prime")]
+        public string Prime([FromBody] int n)
+        {
+            if (n < 2) return "Not a prime number";
+            bool isPrime = false;
+            for (int i = 2; i*i <= n; i++)
+            {
+                if(n % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+                isPrime = true;
+            }
+            return isPrime ? "A prime number" : "Not a prime number";
+        }
+    }
+}
